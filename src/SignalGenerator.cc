a0446eaa22b81b475f227ae82e4e982914fceffa
lennertdekeukeleere <lennert.dekeukeleere@gmail.com>
2022-01-05 18:29:28 +0100

    debugging

--- src/SignalGenerator.cc
+++ src/SignalGenerator.cc
@@ -581,7 +581,7 @@ void SignalGenerator::Run() {
                   double xe2, ye2, ze2, te2;
                   int status;
                   fDrift->DriftElectron(xe,ye,ze,te);
-                  fDrift->GetElectronEndpoint(fDrift->GetNumberOfElectronEndpoints()-1, xe1, ye1, ze1, te1, xe2, ye2, ze2, te2, status);
+                  fDrift->GetElectronEndpoint(fDrift->GetNumberOfElectronEndpoints(), xe1, ye1, ze1, te1, xe2, ye2, ze2, te2, status);
                   if(WireIsHit(status,xe2,ye2,ze2)) {
                     nDetEl++;
                   }
