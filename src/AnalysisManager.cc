ff4db760c919f98edc508d70325d8451662372cc
lennertdekeukeleere <lennert.dekeukeleere@gmail.com>
2022-01-05 23:59:59 +0100

    debugging

--- src/AnalysisManager.cc
+++ src/AnalysisManager.cc
@@ -100,6 +100,7 @@ void AnalysisManager::Write(){
 }
 
 double AnalysisManager::GetTDC(TH1D* hist, double factor, double thresh_level){
+	std::cout << thresh_level << std::endl;
 	for(int i = 1; i<=hist->GetSize()-2; i++){
 		if(hist->GetBinContent(i)*factor>thresh_level)
 			return hist->GetBinLowEdge(i);
